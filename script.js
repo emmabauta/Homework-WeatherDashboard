3e475bea8c2100024e2d358e6016fd9c4c00306b
DillEmma <emmabauta@gmail.com>
2021-01-06 22:30:55 -0500

    Ajax for forecast

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ $("#search-value").val("")
 
 searchWeather(searchValue)
 })
-// Ajax call to pull weather data
+// Ajax call to pull weather data for current day 
 function searchWeather(searchValue){
     $.ajax({
         method: "GET",
@@ -17,6 +17,19 @@ function searchWeather(searchValue){
         dataType: "json",
         success: function(data){
             console.log(data);
+            $("#current-weather").append(JSON.stringify(data));
+        }
+    })
+}
+// Ajax call to pull weather for 5 day forecast 
+function searchWeather(searchValue){
+    $.ajax({
+        method:"GET",
+        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=" + APIkey + "&units=imperial",
+        dataType: "json", 
+        success: function(data){
+            console.log(data);
+            $("#forecast-weather").append(JSON.stringify(data));
         }
     })
 }
